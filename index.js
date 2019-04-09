0f3e7fcc37c9fba3566d65058fa7e4a8ef637633
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-09 19:05:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo(holiday) {
+return `Happy ${holiday}, to ${name}!`
+}
+
+function happyCountdown(days, holiday){
+return `It's ${days} until ${holiday}!`
+}
